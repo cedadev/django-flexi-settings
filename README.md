d58004404f7cd2777917628b0f059451ad2af6d8
Matt Pryor <matt.pryor@stfc.ac.uk>
2020-02-20 12:49:30 +0000

    Initial commit with Python, YAML and JSON supported

---
+++ README.md
@@ -0,0 +1,3 @@
+# django-flexi-settings
+
+Package that allows flexible settings configuration for Django projects.
---
+++ flexi_settings/loaders.py
@@ -0,0 +1,105 @@
+"""
+Module defining built-in settings loaders.
+"""
+
+import importlib.util
+import pathlib
+import pkg_resources
+
+
+class NoAvailableLoader(RuntimeError):
+    """
+    Raised when asked to load a file with an unknown extension.
+    """
+    def __init__(self, path):
+        super().__init__(f'No available loader for {path}')
+
+
+def get_available_loaders(entry_point = 'flexi_settings.loaders'):
+    """
+    Discovers the available loaders using the given entry point.
+    """
+    loaders = {}
+    for entry_point in pkg_resources.iter_entry_points(entry_point):
+        loader = entry_point.load()
+        loaders.update({ ext: loader for ext in loader.extensions })
+    return loaders
+
+
+def include(path, settings):
+    """
+    Includes the given settings file and merges into the given settings.
+    """
+    path = pathlib.Path(path)
+    try:
+        loader = get_available_loaders()[path.suffix]
+    except KeyError:
+        raise NoAvailableLoader(path)
+    else:
+        loader(path, settings)
+
+
+def include_dir(path, settings):
+    """
+    Includes each settings file from the given directory, in lexicographical order,
+    and merges them into the given settings.
+    """
+    path = pathlib.Path(path)
+    # Iterate the files in the directory and attempt to load each one
+    for item in sorted(path.iterdir()):
+        include(item, settings)
+
+
+def load_python(path, settings):
+    """
+    Loads settings from a Python file and merges with the given settings.
+    """
+    with open(path, 'r') as fh:
+        code = compile(fh.read(), path, mode = 'exec')
+    # Override __file__ for the duration of the exec
+    old_file = settings.get('__file__')
+    settings['__file__'] = str(path)
+    exec(code, settings)
+    settings['__file__'] = old_file
+load_python.extensions = { '.py', '.conf' }
+
+
+def merge_settings(settings, overrides):
+    """
+    Deep-merge overrides into settings.
+    """
+    for key, value in overrides.items():
+        if isinstance(value, dict):
+            merge_settings(settings.setdefault(key, {}), value)
+        else:
+            settings[key] = value
+
+
+def load_yaml(path, settings):
+    """
+    Loads settings from a YAML file and merges with the given settings.
+
+    The YAML file should contain a dictionary, which is merged with the existing settings.
+
+    The dictionary keys can be UPPER_CASE, lower_case or snakeCase, and are normalised to UPPER_CASE.
+    """
+    import yaml
+    with open(path, 'r') as fh:
+        overrides = yaml.safe_load(fh)
+    merge_settings(settings, overrides)
+load_yaml.extensions = { '.yaml', '.yml' }
+
+
+def load_json(path, settings):
+    """
+    Loads settings from a JSON file and merges with the given settings.
+
+    The JSON file should contain an object, which is merged with the existing settings.
+
+    The object keys can be UPPER_CASE, lower_case or snakeCase, and are normalised to UPPER_CASE.
+    """
+    import json
+    with open(path, 'r') as fh:
+        overrides = json.load(fh)
+    merge_settings(settings, overrides)
+load_json.extensions = { '.json' }
